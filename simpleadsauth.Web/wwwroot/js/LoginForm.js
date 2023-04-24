1406ef84660cf5cf7c62a54e8aeb78b2d62a5c9f
Sara Reiss <Sarareiss003@gmail.com>
2023-04-23 22:28:44 -0400

    added java script

---
+++ simpleadsauth.Web/wwwroot/js/LoginForm.js
@@ -0,0 +1,16 @@
+﻿$(() => {
+
+      $("input").on('input', function () {
+        ensureFormIsValid();
+
+    });
+
+ function ensureFormIsValid() {
+        const email = $("#email").val().trim();
+        const password = $("#password").val().trim();
+         let isValid = email && password;
+        $("#login").prop('disabled', !isValid);
+
+    }
+
+});
---
+++ simpleadsauth.Web/wwwroot/js/signupform.js
@@ -0,0 +1,24 @@
+﻿$(() => {
+
+
+    $("input").on('input', function () {
+        ensureFormIsValid();
+
+    });
+
+   
+
+   
+    function ensureFormIsValid() {
+        const name = $("#name").val().trim();
+        const number = $("#number").val().trim();
+        const email = $("#email").val().trim();
+        const password = $("#password").val().trim();
+        
+
+        let isValid = name && number && email && password;
+        $("#sign-up").prop('disabled', !isValid);
+
+    }
+
+});
